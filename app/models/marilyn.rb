1ee6cf51c0db3366d17e43996b01d0f133fc2568
Benj <benj@rocksbox.com>
2018-03-21 10:16:57 -0700

    More logs

--- app/models/marilyn.rb
+++ app/models/marilyn.rb
@@ -2,6 +2,7 @@ require 'net/http'
 
 class Marilyn < ApplicationRecord
   def self.set_light(urgency)
+    p "Marilyn is in the building"
     color = case urgency
             when 0
               '1'
@@ -11,8 +12,9 @@ class Marilyn < ApplicationRecord
               '44536'
             end
     hue_url = ENV['HUE_AUTH'] + '/lights/12/state'
+    p hue_url
     req = Net::HTTP::Put.new(hue_url)
-
+    p ENV['HUE_HOST']
     req.body = '{"on":true, "bri":254,"hue": ' + color + '}'
     p "Sending call to light"
     response = Net::HTTP.new(ENV['HUE_HOST']).start {|http| http.request(req) }
