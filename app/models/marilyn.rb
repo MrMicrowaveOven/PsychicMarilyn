27ef74e8239b4e5d808db5cadae611151ab40632
Benj <benj@rocksbox.com>
2018-03-21 01:14:07 -0700

    Added Marilyn, which controls hue lights

---
+++ app/models/marilyn.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+
+class Marilyn < ApplicationRecord
+  def self.set_light(urgency)
+    color = case urgency
+            when 0
+              '1'
+            when 1
+              '50536'
+            when 2
+              '44536'
+            end
+    hue_url = ENV['HUE_AUTH'] + '/lights/12/state'
+    req = Net::HTTP::Put.new(hue_url)
+
+    req.body = '{"on":true, "bri":254,"hue": ' + color + '}'
+
+    response = Net::HTTP.new(ENV['HUE_HOST']).start {|http| http.request(req) }
+    response.body
+  end
+end
---
+++ test/models/marilyn_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class MarilynTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
